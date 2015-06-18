0fbfe81ab497bea012c39d18deedfd30d6a6cef7
Justin Clark-Casey <justincc@intermine.org>
2015-06-18 16:07:00 +0100

    Remove stub fetchEggNogData perl file in favour of a stub python implementation
    Not best for consistency but don't really want to keep writing new perl

---
+++ eggNog/fetchEggNogData.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+import argparse
+
+parser = argparse.ArgumentParser('Retrieve required EggNOG files and filter required data by organism taxon IDs.')
+args = parser.parse_args()
