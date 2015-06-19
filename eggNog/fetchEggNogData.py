c788a4f16580952524c17b99b550a7fac61164ec
Justin Clark-Casey <justincc@intermine.org>
2015-06-19 11:17:33 +0100

    pass in ftp and file details to the assemblePrereqFiles function rather than having them be global

--- eggNog/fetchEggNogData.py
+++ eggNog/fetchEggNogData.py
@@ -10,13 +10,9 @@ import os.path
 def logEyeCatcher(text):
   print "~~~ %s ~~~" % text
 
-def assemblePrereqFiles():
+def assemblePrereqFiles(ftpHost, ftpBase, files):
   logEyeCatcher("Checking pre-requisite data files")
 
-  eggNogFtpHost = 'eggnog.embl.de'
-  eggNogFtpPath = 'eggNOG/4.0/'
-
-  files = set(['eggnogv4.funccats.txt', 'description/bactNOG.description.txt.gz', 'funccat/bactNOG.funccat.txt.gz', 'members/bactNOG.members.txt.gz', 'id_conversion.tsv'])
   missingFiles = set()
 
   # TODO: If need be this can be more sophisticated to check things like file sizes local vs ftp
@@ -30,9 +26,9 @@ def assemblePrereqFiles():
   if len(missingFiles) > 0:
     print "Downloading required files from %s.  WARNING: One or more of these may be very large files (>4G)" % eggNogFtpHost
 
-    ftp = ftplib.FTP(eggNogFtpHost)
+    ftp = ftplib.FTP(ftpHost)
     ftp.login()
-    ftp.cwd(eggNogFtpPath)
+    ftp.cwd(ftpBase)
 
     for f in missingFiles:
       fbn = os.path.basename(f)
@@ -60,6 +56,10 @@ parser = argparse.ArgumentParser('Retrieve required EggNOG files and filter requ
 parser.add_argument('taxonIdsPath', help = 'path to the taxon IDs file')
 args = parser.parse_args()
 
-assemblePrereqFiles()
+eggNogFtpHost = 'eggnog.embl.de'
+eggNogFtpBase = 'eggNOG/4.0/'
+files = set(['eggnogv4.funccats.txt', 'description/bactNOG.description.txt.gz', 'funccat/bactNOG.funccat.txt.gz', 'members/bactNOG.members.txt.gz', 'id_conversion.tsv'])
+
+assemblePrereqFiles(eggNogFtpHost, eggNogFtpBase, files)
 taxonIds = readTaxonIds(args.taxonIdsPath)
 # print "taxons [%s]" % (" ".join(taxonIds))
