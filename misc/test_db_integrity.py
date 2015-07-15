5433435267792ac1c384147b03e6ad90cfd54e1c
Justin Clark-Casey <justincc@intermine.org>
2015-07-15 13:21:29 +0100

    Add section to test_db_integrity.py to print out all location.locatedonid

--- misc/test_db_integrity.py
+++ misc/test_db_integrity.py
@@ -11,5 +11,11 @@ cur = conn.cursor()
 cur.execute("select count(*) from intermineobject;")
 print "%s has %s InterMine objects" % (dbName, cur.fetchone()[0])
 
+cur.execute("select distinct locatedonid from location order by locatedonid;")
+for row in cur:
+  locatedonid = row[0]
+  if locatedonid != None:
+    print "Found location.locatedonid %s" % locatedonid
+
 cur.close()
 conn.close()
