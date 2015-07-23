b8bd2b5fcd80fd85ce9b85922ad4e2165bf56cd6
Justin Clark-Casey <justincc@intermine.org>
2015-07-23 16:37:38 +0100

    Add scratch script to compare difference in results for a particular template between mines in detail

---
+++ misc/compare_template_in_detail.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+
+import argparse
+import sys
+from intermine.webservice import Service
+
+class MyParser(argparse.ArgumentParser):
+    def error(self, message):
+        sys.stderr.write('error: %s\n' % message)
+        self.print_help()
+        sys.exit(2)
+
+############
+### MAIN ###
+############
+parser = MyParser('Compare a template between two versions of the same webservice in detail.')
+parser.add_argument('template_name')
+parser.add_argument('service_version_a')
+parser.add_argument('service_version_b')
+args = parser.parse_args()
+
+serviceA = Service(args.service_version_a)
+templateA = serviceA.get_template(args.template_name)
+
+"""
+for c in templateA.editable_constraints:
+  print c
+"""
+
+rowsA = templateA.get_row_list()
+strsA = map(str, rowsA)
+
+serviceB = Service(args.service_version_b)
+templateB = serviceB.get_template(args.template_name)
+rowsB = templateB.get_row_list()
+strsB = map(str, rowsB)
+
+countInAOnly = 0
+countInBOnly = 0
+
+for rowA in rowsA:
+  if rowA['Gene.symbol'] == 'acoR':
+    print rowA
+
+for rowB in rowsB:
+  if rowB['Gene.symbol'] == 'acoR':
+    print rowB
+
+"""
+# Very crude comparion
+for strA in strsA:
+  print strA
+  if strA in strsB:
+    countInBOnly += 1
+
+for strB in strsB:
+  if strB not in strsA:
+    countInAOnly += 1
+
+print "Results found in A only: %d" % countInAOnly
+print "Results found in B only: %d" % countInBOnly
+"""
