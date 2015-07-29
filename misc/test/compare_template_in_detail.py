2b001c35a9624c35928317f71b17f82bf2465247
Justin Clark-Casey <justincc@intermine.org>
2015-07-29 13:17:19 +0100

    factor out crude comparison routine code

--- misc/test/compare_template_in_detail.py
+++ misc/test/compare_template_in_detail.py
@@ -4,11 +4,54 @@ import argparse
 import sys
 from intermine.webservice import Service
 
+###############
+### CLASSES ###
+###############
 class MyParser(argparse.ArgumentParser):
     def error(self, message):
         sys.stderr.write('error: %s\n' % message)
         self.print_help()
         sys.exit(2)
+
+###################
+### SUBROUTINES ###
+###################
+def compareCrude(rowsA, rowsB):
+  strsA = map(str, rowsA)
+  strsB = map(str, rowsB)
+
+  countInAOnly = 0
+  countInBOnly = 0
+  countInBoth = 0
+
+  """
+  for rowA in rowsA:
+    if rowA['Gene.symbol'] == 'acoR':
+      print rowA
+
+  for rowB in rowsB:
+    if rowB['Gene.symbol'] == 'acoR':
+      print rowB
+  """
+
+  # Very crude comparison
+  print '### RESULTS FOUND IN SERVICE A ONLY ###'
+  for strA in strsA:
+    if strA not in strsB:
+      print strA
+      countInAOnly += 1
+    else:
+      countInBoth += 1
+
+  print '### RESULTS FOUND IN SERVICE B ONLY ###'
+  for strB in strsB:
+    if strB not in strsA:
+      print strB
+      countInBOnly += 1
+
+  print "Results found in service A only: %d" % countInAOnly
+  print "Results found in service B only: %d" % countInBOnly
+  print "Results found in both: %d" % countInBoth
 
 ############
 ### MAIN ###
@@ -21,49 +64,10 @@ args = parser.parse_args()
 
 serviceA = Service(args.service_version_a)
 templateA = serviceA.get_template(args.template_name)
-
-"""
-for c in templateA.editable_constraints:
-  print c
-"""
-
 rowsA = templateA.get_row_list()
-strsA = map(str, rowsA)
 
 serviceB = Service(args.service_version_b)
 templateB = serviceB.get_template(args.template_name)
 rowsB = templateB.get_row_list()
-strsB = map(str, rowsB)
 
-countInAOnly = 0
-countInBOnly = 0
-countInBoth = 0
-
-"""
-for rowA in rowsA:
-  if rowA['Gene.symbol'] == 'acoR':
-    print rowA
-
-for rowB in rowsB:
-  if rowB['Gene.symbol'] == 'acoR':
-    print rowB
-"""
-
-# Very crude comparison
-print '### RESULTS FOUND IN SERVICE A ONLY ###'
-for strA in strsA:
-  if strA not in strsB:
-    print strA
-    countInAOnly += 1
-  else:
-    countInBoth += 1
-
-print '### RESULTS FOUND IN SERVICE B ONLY ###'
-for strB in strsB:
-  if strB not in strsA:
-    print strB
-    countInBOnly += 1
-
-print "Results found in service A only: %d" % countInAOnly
-print "Results found in service B only: %d" % countInBOnly
-print "Results found in both: %d" % countInBoth
+compareCrude(rowsA, rowsB)
