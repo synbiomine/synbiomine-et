27985998bb12f6c3e7a52a5e1a88e7b21ddfdf3f
Justin Clark-Casey <justincc@intermine.org>
2015-08-19 18:06:15 +0100

    print count of retrieved pathways

--- ecocyc/fetchPathwaysData.py
+++ ecocyc/fetchPathwaysData.py
@@ -17,15 +17,15 @@ conn = httplib.HTTPConnection("biocyc.org")
 conn.request("GET", "/ECOLI/class-instances?object=Pathways")
 resp = conn.getresponse()
 print resp.status, resp.reason
-# print "[%s]" % resp.read()
+
+pathways = set()
 
 soup = BeautifulSoup(resp.read(), 'html.parser')
+conn.close()
 
 for tag in soup.find_all(href=re.compile("type=PATHWAY")):
   uhref = urllib.unquote(tag['href'])
   m = pathwayRe.search(uhref)
-  print m.group(1)
-  # print tag['href']
-  # tag['href']
+  pathways.add(m.group(1))
 
-conn.close()
+print "Got %d pathways" % (len(pathways))
