8304fa02da7de1e886e8b96ebee028f77fbf31b3
Justin Clark-Casey <justincc@intermine.org>
2015-08-19 17:58:02 +0100

    print pathway tags

--- ecocyc/fetchPathwaysData.py
+++ ecocyc/fetchPathwaysData.py
@@ -1,10 +1,27 @@
 #!/usr/bin/python
 
-import httplib
+# requires
+#   pip install beautifulsoup4
 
-c = httplib.HTTPConnection("biocyc.org")
-c.request("GET", "ECOLI/class-instances?object=Pathways")
-r = c.getresponse()
-print r.status, r.reason
-print "[%s]" % r.read()
-c.close()
+import httplib
+import re
+import urllib
+from bs4 import BeautifulSoup
+
+############
+### MAIN ###
+############
+# pathwayRe = re.compile("object=(
+conn = httplib.HTTPConnection("biocyc.org")
+conn.request("GET", "/ECOLI/class-instances?object=Pathways")
+resp = conn.getresponse()
+print resp.status, resp.reason
+# print "[%s]" % resp.read()
+
+soup = BeautifulSoup(resp.read(), 'html.parser')
+
+for tag in soup.find_all(href=re.compile("type=PATHWAY")):
+  print tag
+  # tag['href']
+
+conn.close()
