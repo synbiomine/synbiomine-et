58ed9a10ae6c7d0921420552e6eb7ea8e6fbbbef
Justin Clark-Casey <justincc@intermine.org>
2015-08-25 16:56:31 +0100

    Factor out pathways.dat reading code to processPathwaysDatFile()

--- ecocyc/ecocyc_pathways_flat_files_2itemsXML.py
+++ ecocyc/ecocyc_pathways_flat_files_2itemsXML.py
@@ -38,6 +38,7 @@ Process the file linking pathways to genes
 Returns a data structure with the format:
 { 
   'UNIQUE-ID' : 
+    'UNIQUE-ID' : [ <id> ]
     'NAME' : [ <name> ]
     'GENE-NAME' : [ <gene-name>* ]
     'GENE-ID' : [ <gene-id>* ]
@@ -82,6 +83,95 @@ def processPathwaysColFile(fn):
 
   return pathways
 
+"""
+Process the pathways information file
+
+Returns a data structure with the format:
+{ 
+  'UNIQUE-ID' : 
+    'UNIQUE-ID' : [ <id> ]
+    'COMMON-NAME' : [ <name> ]
+    'COMMENT' : [ <gene-name>* ]
+    * : [ * ]
+}
+"""
+def processPathwaysDatFile(fn):
+  pathways = {}
+
+  with open("%s/%s" % (inputDn, pathwaysDatFn)) as f:
+    # Used to add continued lines
+    lastKey = None
+
+    pathway = None
+
+    for line in f:
+      line = line.strip(os.linesep)
+      line = urllib.unquote(line)
+
+      # Ignore comments
+      if line.startswith('#'):
+        continue
+
+      # Terminate record
+      if line == '//':
+        if pathway == None:
+          print >> sys.stderr, "Found record end with no associated pathway."
+          sys.exit(1)
+        else:
+          # print "Ending record for pathway %s" % pathway['UNIQUE-ID'][0]
+
+          pathways[pathway['UNIQUE-ID'][0]] = pathway
+          pathway = None
+          lastKey = None
+          continue
+
+      # Process continued line
+      if line.startswith('/'):
+        if pathway == None:
+          print >> sys.stderr, "Found continued line but with no current pathway."
+          sys.exit(1)
+        if lastKey == None:
+          print >> sys.stderr, "Found continued line for pathway %s but with no last key." % pathway['UNIQUE-ID']
+          sys.exit(1)
+        else:
+          line = " " + line[1:]
+          pathway[lastKey][-1] += line
+          continue
+
+      # Complain if we now have a line not containing '-'
+      # All such valid lines would be dealt with by the logic above
+      if not '-' in line:
+        print >> sys.stderr, "Did not find '-' in line [%s]" % line
+        sys.exit(1)
+
+      (key, value) = line.split(' - ', 1)
+      # print "%s:%s" % (key, value)
+
+      # Setup new pathway record
+      if key == 'UNIQUE-ID':
+        # Sanity
+        if pathway != None:
+          print >> sys.stderr, "Found new UNIQUE-ID %s but still have unfinished pathway %s" % (value, pathway['UNIQUE-ID'])
+          sys.exit(1)
+
+        print "Processing pathway %s" % value
+
+        pathway = {}
+
+      # Add value to key entry
+      if not key in pathway:
+        pathway[key] = []
+
+      pathway[key].append(value)
+
+      lastKey = key
+
+  if pathway != None:
+    print >> sys.stderr, "Finished processing %s but still in unclosed pathway %s" % (pathwaysDatFn, pathway['UNIQUE-ID'])
+    sys.exit(1)
+
+  return pathways
+
 ############
 ### MAIN ###
 ############
@@ -103,82 +193,8 @@ if not os.path.isdir(args.inputDirname):
 inputDn = args.inputDirname
 outputFn = args.outputFilename
 
-# We're going to resolve by symbol for now.  Organism name will always be the same
-processPathwaysColFile("%s/%s" % (inputDn, pathwaysColFn))
-
-pathways = {}
-
-with open("%s/%s" % (inputDn, pathwaysDatFn)) as f:
-  # Used to add continued lines
-  lastKey = None
-
-  pathway = None
-
-  for line in f:
-    line = line.strip(os.linesep)
-    line = urllib.unquote(line)
-
-    # Ignore comments
-    if line.startswith('#'):
-      continue
-
-    # Terminate record
-    if line == '//':
-      if pathway == None:
-        print >> sys.stderr, "Found record end with no associated pathway."
-        sys.exit(1)
-      else:
-        # print "Ending record for pathway %s" % pathway['UNIQUE-ID'][0]
-
-        pathways[pathway['UNIQUE-ID'][0]] = pathway
-        pathway = None
-        lastKey = None
-        continue
-
-    # Process continued line
-    if line.startswith('/'):
-      if pathway == None:
-        print >> sys.stderr, "Found continued line but with no current pathway."
-        sys.exit(1)
-      if lastKey == None:
-        print >> sys.stderr, "Found continued line for pathway %s but with no last key." % pathway['UNIQUE-ID']
-        sys.exit(1)
-      else:
-        line = " " + line[1:]
-        pathway[lastKey][-1] += line
-        continue
-
-    # Complain if we now have a line not containing '-'
-    # All such valid lines would be dealt with by the logic above
-    if not '-' in line:
-      print >> sys.stderr, "Did not find '-' in line [%s]" % line
-      sys.exit(1)
-
-    (key, value) = line.split(' - ', 1)
-    # print "%s:%s" % (key, value)
-
-    # Setup new pathway record
-    if key == 'UNIQUE-ID':
-      # Sanity
-      if pathway != None:
-        print >> sys.stderr, "Found new UNIQUE-ID %s but still have unfinished pathway %s" % (value, pathway['UNIQUE-ID'])
-        sys.exit(1)
-
-      print "Processing pathway %s" % value
-
-      pathway = {}
-
-    # Add value to key entry
-    if not key in pathway:
-      pathway[key] = []
-
-    pathway[key].append(value)
-
-    lastKey = key
-
-if pathway != None:
-  print >> sys.stderr, "Finished processing %s but still in unclosed pathway %s" % (pathwaysDatFn, pathway['UNIQUE-ID'])
-  sys.exit(1)
+pathways = processPathwaysDatFile("%s/%s" % (inputDn, pathwaysDatFn))
+pathwaysToGenes = processPathwaysColFile("%s/%s" % (inputDn, pathwaysColFn))
 
 print "Processed %d pathways" % len(pathways)
 
