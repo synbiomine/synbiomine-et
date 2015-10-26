2f415ff5d8c703b511445a77701eac0f3db7606a
Justin Clark-Casey <justincc@intermine.org>
2015-10-26 16:06:31 +0000

    refactor: move code scraping parts to a function

--- igem/fetchIgemMetadata.py
+++ igem/fetchIgemMetadata.py
@@ -43,6 +43,44 @@ def prettyPrint(parts):
 
   print summaryTable.draw()
 
+def scrapeParts(host, url):
+  conn = httplib.HTTPConnection(host)
+
+  print "Fetching http://%s%s" % (host, url)
+  conn.request("GET", url)
+  resp = conn.getresponse()
+
+  # We have to use html5lib here because the iGEM pages are malformed (e.g. all <tr>s inside the last <th> tag because it wasn't closed - something a browser corrects automatically
+  soup = BeautifulSoup(resp.read(), 'html5lib')
+  conn.close()
+
+  # Multiple tables with this id on a page
+  tables = soup.find_all('table', id='assembly_plasmid_table')
+  parts = []
+
+  for table in tables:
+    # print '### TABLE ###'
+
+    for row in table.tbody.find_all('tr')[1:-1]:
+      cols = row.find_all('td')
+
+      if args.verbose:
+        print 'Processing row',
+
+      part = Part()
+      part.name = cols[0].a.contents[0].strip()
+
+      if args.verbose:
+        print part.name
+
+      part.proteinName = cols[1].contents[0].strip()
+      part.description = cols[2].contents[0].strip()
+      part.uniprotName = cols[5].contents[0].strip()
+
+      parts.append(part)
+
+  return parts
+
 ############
 ### MAIN ###
 ############
@@ -53,39 +91,5 @@ parser = MyParser('Scrape iGEM part metadata.')
 parser.add_argument('-v', '--verbose', action='store_true')
 args = parser.parse_args()
 
-conn = httplib.HTTPConnection(host)
-
-print "Fetching http://%s%s" % (host, url)
-conn.request("GET", url)
-resp = conn.getresponse()
-
-# We have to use html5lib here because the iGEM pages are malformed (e.g. all <tr>s inside the last <th> tag because it wasn't closed - something a browser corrects automatically
-soup = BeautifulSoup(resp.read(), 'html5lib')
-conn.close()
-
-# Multiple tables with this id on a page
-tables = soup.find_all('table', id='assembly_plasmid_table')
-parts = []
-
-for table in tables:
-  # print '### TABLE ###'
-
-  for row in table.tbody.find_all('tr')[1:-1]:
-    cols = row.find_all('td')
-
-    if args.verbose:
-      print 'Processing row',
-
-    part = Part()
-    part.name = cols[0].a.contents[0].strip()
-
-    if args.verbose:
-      print part.name
-
-    part.proteinName = cols[1].contents[0].strip()
-    part.description = cols[2].contents[0].strip()
-    part.uniprotName = cols[5].contents[0].strip()
-
-    parts.append(part)
-
+parts = scrapeParts(host, url)
 prettyPrint(parts)
