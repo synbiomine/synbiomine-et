fee3ee4a4828952916bb6b0fac9a76ba5c02191e
Justin Clark-Casey (justincc) <justinccdev@gmail.com>
2016-01-15 15:06:14 +0000

    amend synbiomine direct query

--- examples/scripts/python/synbiomine-direct-fasta-query.py
+++ examples/scripts/python/synbiomine-direct-fasta-query.py
@@ -4,7 +4,11 @@ import requests
 
 #endpoint = "http://synbiomine.org/query/service/query/results"
 endpoint = 'http://www.flymine.org/query/service/query/results/fasta'
-query = '<query name="short genes on the X chromosome" model="genomic" view="Gene.id"> <constraint path="Gene.chromosome.primaryIdentifier" op="=" value="X"/> <constraint path="Gene.length" op="&lt;" value="1000"/> </query>'
+# query = '<query name="short genes on the X chromosome" model="genomic" view="Gene.id"> <constraint path="Gene.chromosome.primaryIdentifier" op="=" value="X"/> <constraint path="Gene.length" op="&lt;" value="1000"/> </query>'
+query = '''<query model="genomic" view="Gene.id">
+<constraint path="Gene.organism.taxonId" op="LOOKUP" value="224308"/>
+<constraint path="Gene.length" op="&lt;" value="1000"/>
+</query>'''
 params = {'query':query}
 
 response = requests.post(endpoint, params=params)
