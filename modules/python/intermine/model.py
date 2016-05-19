56255b7d27964910f6efb58b1704d02c9cbebee7
Justin Clark-Casey <justincc@intermine.org>
2016-05-19 18:03:30 +0100

    Add and move documentation in python/intermine/model.py

--- modules/python/intermine/model.py
+++ modules/python/intermine/model.py
@@ -13,23 +13,33 @@ class Document:
     self._items = []
 
   def addItem(self, item):
+    """
+    Add an item to this document.
+
+    Items can be changed after they are added (e.g. additional references added to a collection)
+    :param item:
+    :return:
+    """
+    
     self._items.append(item)
 
-  """
-  Create an item in the given document.
-  This factory method should always be used rather than the constructor.
-  """
   def createItem(self, className):
+    """
+    Create an item in the given document.
+    This factory method should always be used rather than the constructor.
+    """
+
     item = Item(self._model, className)
     item._id = "0_%d" % self._nextItemNumber
     self._nextItemNumber += 1
 
     return item
 
-  """
-  Write the document to the filesystem
-  """
   def write(self, outFn):
+    """
+    Write the document to the filesystem
+    """
+
     itemsTag = etree.Element("items")   
     
     for item in self._items:
@@ -57,19 +67,21 @@ class Item:
 
     self._attrs = {}
 
-  """
-  Add an attribute to this item.
-  If the value is empty then nothing is added since InterMine doesn't like this behaviour.
-  """
   def addAttribute(self, name, value):
+    """
+    Add an attribute to this item.
+    If the value is empty then nothing is added since InterMine doesn't like this behaviour.
+    """
+
     if value != "":
       self._attrs[name] = value
 
-  """
-  Add a value to an attribute.  If the attribute then has more than one value it becomes a collection.
-  An attribute that doesn't already exist becomes a collection with a single value.
-  """
   def addToAttribute(self, name, value):
+    """
+    Add a value to an attribute.  If the attribute then has more than one value it becomes a collection.
+    An attribute that doesn't already exist becomes a collection with a single value.
+    """
+
     if name in self._attrs:
       self._attrs[name].append(value)
     else:
