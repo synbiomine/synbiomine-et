c5dd7fbf2fcde3e32a7d8aa68874a81f9e5cf9cc
Justin Clark-Casey <justincc@intermine.org>
2016-05-25 17:00:43 +0100

    Replace writePubMedProjectXML.pl with writePubMedProjectXml.py for future simplicity and maintainability

---
+++ pubmed/writePubMedProjectXml.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import shutil
+
+sys.path.insert(1, os.path.dirname(os.path.abspath(__file__)) + '/../modules/python')
+import intermine.project as imp
+import intermine.utils as imu
+
+############
+### MAIN ###
+############
+parser = imu.ArgParser('Add PubMed source entry to InterMine SynBioMine project XML.')
+parser.add_argument('datasetPath', help='path to the dataset location.')
+parser.add_argument('pubMedDataPath', help='path to gene2pubmed file.')
+parser.add_argument('-v', '--verbose', action="store_true", help="verbose output")
+args = parser.parse_args()
+
+datasetPath = args.datasetPath
+pubMedDataPath = args.pubMedDataPath
+
+logPath = "%s/logs/writeOrthoDbProjectXml.log" % datasetPath
+sys.stdout = imu.Logger(logPath)
+projectXmlPath = "%s/intermine/project.xml" % datasetPath
+taxonsPath = "%s/taxons/taxons.txt" % datasetPath
+
+with open(taxonsPath) as f:
+    taxons = f.read().strip()
+
+project = imp.Project("%s/intermine/project.xml" % datasetPath)
+
+source = imp.Source(
+    'pubmed-gene', 'pubmed-gene',
+    [
+        { 'name':'src.data.dir',            'location':os.path.dirname(pubMedDataPath) },
+        { 'name':'src.data.dir.includes',   'value':os.path.basename(pubMedDataPath) },
+        { 'name':'pubmed.organisms',        'value':taxons }
+    ],
+    dump=True)
+project.addSource(source)
+
+shutil.copy(projectXmlPath, "%s.bak" % projectXmlPath)
+project.write(projectXmlPath)
