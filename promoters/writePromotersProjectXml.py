0551d36cb91d792ee6427565b379cfce964e59b9
Justin Clark-Casey <justincc@intermine.org>
2016-06-01 16:56:17 +0100

    Add promoters/writePromotersProjectXml.py

---
+++ promoters/writePromotersProjectXml.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+sys.path.insert(1, os.path.dirname(os.path.abspath(__file__)) + '/../modules/python')
+import intermine.project as imp
+import intermine.utils as imu
+
+############
+### MAIN ###
+############
+parser = imu.ArgParser('Add promoter source entries to InterMine SynBioMine project XML.')
+parser.add_argument('datasetPath', help='path to the dataset location.')
+parser.add_argument('-v', '--verbose', action="store_true", help="verbose output")
+args = parser.parse_args()
+
+datasetPath = args.datasetPath
+
+logPath = "%s/logs/writePromotersProjectXml.log" % datasetPath
+sys.stdout = imu.Logger(logPath)
+projectXmlPath = "%s/intermine/project.xml" % datasetPath
+
+imp.addSourcesToProject(
+    "%s/intermine/project.xml" % datasetPath,
+    [
+        imp.Source(
+            'dbtbs-regulation-Bsub168', 'synbio-regulation',
+            [
+                { 'name':'src.data.dir',        'location':'data/promoters/dbtbs' },
+            ]),
+        imp.Source(
+            'nicolas2012-regulation-Bsub168', 'synbio-regulation',
+            [
+                { 'name':'src.data.dir',        'location':'data/promoters/nicolas-2012' },
+            ])
+    ])
