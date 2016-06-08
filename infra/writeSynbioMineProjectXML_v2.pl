0d6c0c7ed26c53be32cbe6abc06671635ce23b04
Justin Clark-Casey <justincc@intermine.org>
2016-06-08 13:55:04 +0100

    simplify infra/writeSynbioMineProjectXML_v2.pl by removing unused -g gff_config option

--- infra/writeSynbioMineProjectXML_v2.pl
+++ infra/writeSynbioMineProjectXML_v2.pl
@@ -14,10 +14,9 @@ binmode(STDOUT, 'utf8');
 # Silence warnings when printing null fields
 no warnings('uninitialized');
 
-my $usage = "Usage: writeSynbioMineProjectXML.pl [-tvgh] dataset_directory
+my $usage = "Usage: writeSynbioMineProjectXML.pl [-tvh] dataset_directory
 
 synopsis: reads the dataset's genbank directory and writes associated data that's needed for the build.
-Run with the -g option, writes info for the gff_config file. 
 The -t option will generate a tab-separated genomes summary file (see below)
 
 options:
@@ -31,12 +30,11 @@ options:
 
 ";
 
-my (%opts, $tabView, $gffconfig, $insert, $verbose);
+my (%opts, $tabView, $insert, $verbose);
 
 getopts('htgvi:', \%opts);
 defined $opts{"h"} and die $usage;
 defined $opts{"t"} and $tabView++; # tab summary - maps assembly vers to org name & tax ID
-defined $opts{"g"} and $gffconfig++; # writes gff_config.txt file to run dir
 defined $opts{"v"} and $verbose++; # used for debugging
 # defined $opts{"i"} and $insert++;
 # Currently we are always inserting but an option may come back again not to insert
@@ -48,18 +46,11 @@ my $base = $ARGV[0];
 my $genbankdir = "$base/genbank";
 my $insertPath = "$base/intermine/project.xml";
 
-# if they want the config, open the file for writing (-g option)
-if ($gffconfig) {
-  open (GFF_CONF_OUT, ">./gff_config.txt") or die "Can't write file: ./gff_config.txt: $!\n";
-  say "Writing ./gff_config.txt"; # reminds you that it goes to a file
-}
-
 # open the dir they specified
 opendir(DIR, $genbankdir) or die "cannot open dir: $!";
 
 # generate headers if a tab summary is needed (-t option)
 say "taxID\torganism name\tsubdir\tfile prefix" if ($tabView);
-#say GFF_CONF_OUT "taxID: taxname\talt ID\tlocus_tag\tsymbol\tsynonym" if ($gffconfig);
 
 my ($projectXml, $sources_e);
 my $xmlParser = XML::LibXML->new();
@@ -144,70 +135,6 @@ while (my $subdir = readdir DIR) {
 
   my $gffFile = "$largest\.gff";
 
-# some of the next section may not be needed as Julie re-wrote the synbio-gff parser
-# it not picks up most of these fields by default
-
-  if ($gffconfig) {
-    open GFF_IN, "$gbDir/$gffFile" or die "can't open file $gffFile: $!";
-
-    my @gff_line;
-    my ($annotation, $db_Xref, $alt_id, $locus_tag, $symbol, $synonym);
-    while (<GFF_IN>) {
-      chomp;
-
-# GFF header format e.g.
-  # Organism name:  Escherichia coli K-12 MG1655
-  # Taxid:          511145
-
-# E. coli file
-  # ID=gene0;Dbxref=EcoGene:EG11277,GeneID:944742;Name=thrL;gbkey=Gene;gene=thrL;gene_synonym=ECK0001,JW4367;locus_tag=b0001
-# B. subtilis line - most of the strains are like this
-  # ID=gene0;Dbxref=GeneID:939978;Name=dnaA;gbkey=Gene;gene=dnaA;locus_tag=BSU00010
-
-      if ($_ =~ /ID=gene0\;/) {
-	@gff_line = split("\t", $_);
-	$annotation = $gff_line[-1];
-
-	($db_Xref = $1) if ($annotation =~ /.+;Dbxref=(.+?),/);
-	($alt_id = $1) if ($db_Xref =~ /(.+):/);
-	($symbol = $1) if ($annotation =~ /;gene=(.+?)\;/);
-	($synonym = $1) if ($annotation =~ /;gene_synonym=(.+)\;/);
-
-	($locus_tag = $1) if ($annotation =~ /.+;locus_tag=(.+)/);
-	$locus_tag =~ s/;old_locus_tag=.+$// if ($locus_tag);
-
-	if ($locus_tag) {
-	  say GFF_CONF_OUT "$taxID.terms=gene,CDS";
-	  say GFF_CONF_OUT "";
-
-# These parts of the gff annotation are now extracted by default
-# 	  if ($alt_id) {
-# #	    say GFF_CONF_OUT "$taxID.gene.attributes.Dbxref.$alt_id=primaryIdentifier\n",
-# #	      "$taxID.gene.attributes.locus_tag=secondaryIdentifier";
-# 	  }
-# 	  else {
-# #	    say GFF_CONF_OUT "$taxID.gene.attributes.locus_tag=primaryIdentifier";
-# 	  }
-# 	  if ($symbol) {
-# #	    say GFF_CONF_OUT "$taxID.gene.attributes.gene=symbol";
-# 	  }
-# 	  if ($synonym) {
-# #	    say GFF_CONF_OUT "$taxID.gene.attributes.gene_synonym=synonym";
-# 	  }
-#	  say GFF_CONF_OUT "$taxID.CDS.attributes.Name=primaryIdentifier";
-#	  say GFF_CONF_OUT "$taxID.exon.attributes.type=scoreType";
-#	  say GFF_CONF_OUT "$taxID: $taxname\t$alt_id\t$locus_tag\t$symbol\t$synonym";
-	}
-	
-  # #       ($chrm, undef, undef, undef, undef, 
-  # # 	undef, undef, undef, $annotation) = split("\t", $_);
-	last;
-      }
-    }
-
-    close (GFF_IN); # close GFF annotation file
-  }
-
 # generates a tab-delimited summary of the genbank directory
 # Provides mappings of assembly IDs to their organisms
   if ($tabView) {
@@ -220,7 +147,7 @@ while (my $subdir = readdir DIR) {
   my $chrm = "$largest\.fna"; # chromosome fasta
 #  my $fasta = "$largest\.frn"; # RNA fasta - we don't use this any more
 
-  if (not($tabView or $gffconfig)) {
+  if (not $tabView) {
     try_add_source(
       catdir($gbDir, $gffFile), $taxID, "$orgm-gff", sub { gen_gff("$orgm-gff", $taxID, $taxname, $subdir, $gbDir, $gffFile); });
     try_add_source(
@@ -235,8 +162,6 @@ while (my $subdir = readdir DIR) {
     
   closedir(CURR); # close the assembly dir
 }
-
-close (GFF_CONF_OUT) if ($gffconfig); # close the config file if using -g
 
 closedir(DIR); # close the genbank dir
 
@@ -312,21 +237,3 @@ sub gen_chrm {
     </source>
 XML
 }
-
-# We don't integrate this anymore
-# sub fasta_print {
-#   my ($orgm, $taxID, $taxname, $fasta, $gbDir) = @_;
-# 
-#   my $fasta_block = <<EOF;
-#     <source name="$orgm-gene-fasta" type="fasta">
-#       <property name="fasta.taxonId" value="$taxID"/>
-#       <property name="fasta.dataSetTitle" value="$taxname fasta data set for genes"/>
-#       <property name="fasta.dataSourceName" value="NCBI"/>
-#       <property name="fasta.className" value="org.intermine.model.bio.Gene"/>
-#       <property name="fasta.classAttribute" value="primaryIdentifier"/>
-#       <property name="fasta.includes" value="$fasta"/>
-#       <property name="src.data.dir" location="$gbDir/"/>
-#     </source>
-# EOF
-#  say $fasta_block unless ($tabView or $gffconfig);
-# }
