7a680a06205081b0f0ac927139b338999afeeece
Justin Clark-Casey <justincc@intermine.org>
2016-06-08 14:29:01 +0100

    Have createNewDataset.py work directly on the dataset path, not on data repo.
    Leave it up to the user to name their dataset in a sensible fashion (e.g. with datestamp)

--- infra/createNewDataset.py
+++ infra/createNewDataset.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 
-import datetime
 import os
 import shutil
 import sys
@@ -20,19 +19,12 @@ logsDir = "logs"
 parser = imu.ArgParser('Prepare a new dataset structure in the data repository')
 parser.add_argument('projectXmlPath', help='path to the project XML template')
 parser.add_argument('modelPath', help='path to the mine model XML')
-parser.add_argument('repositoryPath', help='path to the data repository')
+parser.add_argument('datasetPath', help='path for the dataset')
 args = parser.parse_args()
 
-repoPath = args.repositoryPath
+datasetPath = args.datasetPath
 templatePath = args.projectXmlPath
 modelPath = args.modelPath
-
-if not os.path.exists(repoPath):
-    os.mkdir(repoPath)
-
-# datasetDirName = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")
-datasetDirName = datetime.datetime.now().strftime("%Y-%m-%d")
-datasetPath = "%s/%s" % (repoPath, datasetDirName)
 
 if os.path.exists(datasetPath):
     raise Exception("Dataset path %s already exists!" % datasetPath)
@@ -50,4 +42,4 @@ shutil.copy(templatePath, projectXmlPath)
 datasetModelPath = "%s/intermine/genomic_model.xml" % datasetPath
 shutil.copy(modelPath, datasetModelPath)
 
-print "Created dataset structure at %s" % (os.path.join(repoPath, datasetDirName))
+print "Created dataset structure at %s" % datasetPath
