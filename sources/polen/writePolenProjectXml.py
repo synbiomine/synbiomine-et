17ff22eadfc0363917dc142ffc0a2ec301b3759c
Justin Clark-Casey <justincc@intermine.org>
2016-07-06 12:57:22 +0100

    Add writePolenProjectXml.py

---
+++ sources/polen/writePolenProjectXml.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+sys.path.insert(1, os.path.dirname(os.path.abspath(__file__)) + '/../modules/python')
+import intermyne.project as imp
+import intermyne.utils as imu
+
+############
+### MAIN ###
+############
+imu.handleSimpleSourceAddProcess(
+    "Polen",
+    [
+        imp.Source(
+            'synbio-polen', 'synbio-polen',
+            [
+                { 'name':'src.data.dir',        'location':'data/polen' },
+            ])
+    ],
+    "writePolenProjectXml")
