b8e02109c3480b3ccf5b4bc3d12730339ede204f
Justin Clark-Casey <justincc@intermine.org>
2016-07-26 14:27:10 +0100

    Add information about whether any alt_ids were also first class IDs in the go file

--- sources/go/parseGoData.py
+++ sources/go/parseGoData.py
@@ -19,7 +19,7 @@ ds = dc.getSet('go')
 
 goPath = "%s/%s" % (ds.getLoadPath(), 'go-basic.obo')
 fcIds = set()
-synonymIds = {}
+synIds = {}
 currentFcId = None
 
 with open(goPath) as f:
@@ -35,14 +35,22 @@ with open(goPath) as f:
                 currentFcId = value
                 fcIds.add(currentFcId)
             elif key == 'alt_id':
-                if value in synonymIds:
-                    print "ERROR: Already found synonym %s => %s when trying to set up %s => %s" % (value, synonymIds[value], value, currentFcId)
+                if value in synIds:
+                    print "ERROR: Already found synonym %s => %s when trying to set up %s => %s" % (value, synIds[value], value, currentFcId)
                     sys.exit(2)
                 else:
-                    synonymIds[value] = currentFcId
+                    synIds[value] = currentFcId
 
 for id in fcIds:
     print id
 
 print "Got %d first class ids" % len(fcIds)
-print "Got %d synonyms" % len(synonymIds)
+print "Got %d synonyms" % len(synIds)
+
+synAndFcCount = 0
+for synId in synIds.keys():
+    if synId in fcIds:
+        synAndFcCount += 1
+
+print "Got %d synonyms that were also first-class IDs" % (synAndFcCount)
+# print synIds['GO:0006350']
