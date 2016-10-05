7bcc9e7bf79db1ed01f7bd5ea51bb3de4d40bef8
Justin Clark-Casey <justincc@intermine.org>
2016-10-05 18:12:59 +0100

    fix line indenting on get_db_stats.py

--- test/get_db_stats.py
+++ test/get_db_stats.py
@@ -13,93 +13,93 @@ import texttable
 ### SUBROUTINES ###
 ###################
 def getCounts(conn, showEmpty = True):
-  """Get counts of all InterMine database tables.
-  
-  Returns a dictionary of <table-name>:<count>
-  
-  conn - open database connection
-  showEmpty - if true then 0 counts are also shown"""
-  
-  with conn.cursor() as cur:
-    cur.execute("select table_name from information_schema.tables where table_schema='public' order by table_schema, table_name;")
-    tables = cur.fetchall()
-    results = {}
-      
-    for table in tables:
-      table = table[0]
-        
-      cur.execute("select count(*) from %s" % table)
-      count = cur.fetchone()[0]
-    
-      if showEmpty or count > 0:
-        results[table] = count
-        # prettySummaryTable.add_row([table, count])
-        # print "%s: %s" % (table, count)
-        
-    return results
-  
-def getMetadataSizes(conn, showEmpty = True):
-  """Get sizes of all InterMine metadata entries
-  
-  Returns a dictionary of <name>:<size>
-  
-  conn - open database connection
-  showEmpty - if true then 0 sizes are also shown"""    
-  with conn.cursor() as cur:
-    cur.execute("select key, length(value), length(blob_value) from intermine_metadata;")
-    entries = cur.fetchall()
-    results = {}
-    
-    for entry in entries:
-      (name, size, blobSize) = entry
-      
-      size = max(size, blobSize)
-      
-      if showEmpty or size > 0:
-        results[name] = size
-      
-  return results                
-  
-def outputJson(name, host, counts, metadataSizes, fileName):
-  now = datetime.datetime.now()
-  jsonData = { 'name' : name, 'host' : host, 'date' : now.isoformat(), 'tables' : counts, 'metadata' : metadataSizes }
-  
-  if fileName == None:
-    fileName = "%s-%s-%s" % (name, host, now.isoformat())
+    """Get counts of all InterMine database tables.
 
-  with open(fileName, 'w') as f:
-    f.write(json.dumps(jsonData, indent=4))
-    
+    Returns a dictionary of <table-name>:<count>
+
+    conn - open database connection
+    showEmpty - if true then 0 counts are also shown"""
+
+    with conn.cursor() as cur:
+        cur.execute("select table_name from information_schema.tables where table_schema='public' order by table_schema, table_name;")
+        tables = cur.fetchall()
+        results = {}
+
+        for table in tables:
+            table = table[0]
+
+            cur.execute("select count(*) from %s" % table)
+            count = cur.fetchone()[0]
+
+            if showEmpty or count > 0:
+                results[table] = count
+                # prettySummaryTable.add_row([table, count])
+                # print "%s: %s" % (table, count)
+
+        return results
+
+def getMetadataSizes(conn, showEmpty = True):
+    """Get sizes of all InterMine metadata entries
+
+    Returns a dictionary of <name>:<size>
+
+    conn - open database connection
+    showEmpty - if true then 0 sizes are also shown"""
+    with conn.cursor() as cur:
+        cur.execute("select key, length(value), length(blob_value) from intermine_metadata;")
+        entries = cur.fetchall()
+        results = {}
+
+        for entry in entries:
+            (name, size, blobSize) = entry
+
+            size = max(size, blobSize)
+
+            if showEmpty or size > 0:
+                results[name] = size
+
+    return results
+
+def outputJson(name, host, counts, metadataSizes, fileName):
+    now = datetime.datetime.now()
+    jsonData = { 'name' : name, 'host' : host, 'date' : now.isoformat(), 'tables' : counts, 'metadata' : metadataSizes }
+
+    if fileName == None:
+        fileName = "%s-%s-%s" % (name, host, now.isoformat())
+
+    with open(fileName, 'w') as f:
+        f.write(json.dumps(jsonData, indent=4))
+
 def generatePrettyPrintTable(headers):
-  table = texttable.Texttable()
-  table.set_deco(texttable.Texttable.VLINES | texttable.Texttable.HLINES)
-  table.add_row(headers)
-  
-  return table  
+    table = texttable.Texttable()
+    table.set_deco(texttable.Texttable.VLINES | texttable.Texttable.HLINES)
+    table.add_row(headers)
+
+    return table
 
 def prettyPrintCounts(counts):
-  """Pretty print counts"""
+    """Pretty print counts"""
 
-  print 'OBJECT COUNTS\n'
+    print 'OBJECT COUNTS\n'
 
-  consoleTable = generatePrettyPrintTable(['Table', 'Entries'])
-  
-  for table in sorted(counts.keys()):
-    consoleTable.add_row([table, counts[table]])
+    consoleTable = generatePrettyPrintTable(['Table', 'Entries'])
 
-  print consoleTable.draw()
-  
+    for table in sorted(counts.keys()):
+        consoleTable.add_row([table, counts[table]])
+
+    print consoleTable.draw()
+
 def prettyPrintMetadataSizes(sizes):
-  """Pretty print metadata sizes"""
+    """Pretty print metadata sizes"""
 
-  print 'METADATA SIZES\n'
+    print 'METADATA SIZES\n'
 
-  consoleTable = generatePrettyPrintTable(['Name', 'Size'])
+    consoleTable = generatePrettyPrintTable(['Name', 'Size'])
 
-  for entry in sorted(sizes.keys()):
-    consoleTable.add_row([entry, sizes[entry]])
+    for entry in sorted(sizes.keys()):
+        consoleTable.add_row([entry, sizes[entry]])
 
-  print consoleTable.draw()  
+    print consoleTable.draw()
 
 ############
 ### MAIN ###
@@ -118,18 +118,18 @@ dbName = args.dbname
 connString = "dbname=%s" % dbName
 
 if args.dbuser:
-  connString += " user=%s" % args.dbuser
+    connString += " user=%s" % args.dbuser
 
 dbHost = 'localhost'
 if args.dbhost:
-  dbHost = args.dbhost
-  connString += " host=%s" % dbHost
+    dbHost = args.dbhost
+    connString += " host=%s" % dbHost
 
 if args.dbport:
-  connString + " port=%s" % args.dbport
+    connString + " port=%s" % args.dbport
 
 if args.dbpass:
-  connString += " password=%s" % args.dbpass
+    connString += " password=%s" % args.dbpass
 
 conn = psycopg2.connect(connString)
 
@@ -139,8 +139,8 @@ metadataSizes = getMetadataSizes(conn, showEmpty = args.all)
 conn.close()
 
 if hasattr(args, 'output'):
-  outputJson(dbName, dbHost, counts, metadataSizes, args.output)
+    outputJson(dbName, dbHost, counts, metadataSizes, args.output)
 else:
-  prettyPrintCounts(counts)
-  print
-  prettyPrintMetadataSizes(metadataSizes)
+    prettyPrintCounts(counts)
+    print
+    prettyPrintMetadataSizes(metadataSizes)
