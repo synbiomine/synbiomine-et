a8896a946514dd6e26cb90c6ab9cbe707cb8696f
Justin Clark-Casey <justincc@intermine.org>
2017-01-09 15:22:34 +0000

    change fetchEggNogData-v4p5.py to use uptodate data collections structure for placing data

--- sources/eggnog/fetchEggNogData-v4p5.py
+++ sources/eggnog/fetchEggNogData-v4p5.py
@@ -6,15 +6,13 @@ import wget
 
 sys.path.insert(1, os.path.dirname(os.path.abspath(__file__)) + '/../../modules/python')
 import intermyne.utils as imu
+import synbio.data as sbd
 
 #################
 ### FUNCTIONS ###
 #################
-def logEyeCatcher(text):
-    print "~~~ %s ~~~" % text
-
 def assemblePrereqFiles(localDir, remoteUrlStub, paths):
-    logEyeCatcher("Downloading files")
+    imu.printSection('Downloading files')
 
     for path in paths:
         remoteUrl = "%s/%s" % (remoteUrlStub, path)
@@ -32,17 +30,17 @@ def assemblePrereqFiles(localDir, remoteUrlStub, paths):
 ### MAIN ###
 ############
 parser = imu.ArgParser('Retrieve required EggNOG files.')
-parser.add_argument('datasetPath', help='path to the dataset location.')
+parser.add_argument('colPath', help='path to the data collection.')
 parser.add_argument('-v', '--verbose', action="store_true", help="verbose output")
 args = parser.parse_args()
 
 beVerbose = args.verbose
-datasetPath = args.datasetPath
-eggNogPath = "%s/eggnog" % datasetPath
-logPath = "%s/logs/fetchEggnogData.log" % datasetPath
-sys.stdout = imu.Logger(logPath)
+
+dc = sbd.Collection(args.colPath)
+ds = dc.getSet('eggnog')
+ds.startLogging(__file__)
 
 eggNogUrlStub = "http://eggnogdb.embl.de/download/eggnog_4.5/"
 files = set(['eggnog4.functional_categories.txt', 'data/bactNOG/bactNOG.annotations.tsv.gz', 'data/bactNOG/bactNOG.members.tsv.gz'])
 
-assemblePrereqFiles(eggNogPath, eggNogUrlStub, files)
+assemblePrereqFiles(ds.getLoadPath(), eggNogUrlStub, files)
