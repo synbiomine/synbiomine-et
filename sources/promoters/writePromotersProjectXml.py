2deb22c095291d186231123905f2d5e36d9423ad
Justin Clark-Casey <justincc@intermine.org>
2017-01-09 16:30:16 +0000

    simplify writePromotersProjectXml.py to use synbio.utils infra rather than its own

--- sources/promoters/writePromotersProjectXml.py
+++ sources/promoters/writePromotersProjectXml.py
@@ -3,26 +3,15 @@
 import os
 import sys
 
-sys.path.insert(1, os.path.dirname(os.path.abspath(__file__)) + '/../modules/python')
+sys.path.insert(1, os.path.dirname(os.path.abspath(__file__)) + '/../../modules/python')
 import intermyne.project as imp
-import intermyne.utils as imu
+import synbio.utils as sbu
 
 ############
 ### MAIN ###
 ############
-parser = imu.ArgParser('Add promoter source entries to InterMine SynBioMine project XML.')
-parser.add_argument('datasetPath', help='path to the dataset location.')
-parser.add_argument('-v', '--verbose', action="store_true", help="verbose output")
-args = parser.parse_args()
-
-datasetPath = args.datasetPath
-
-logPath = "%s/logs/writePromotersProjectXml.log" % datasetPath
-sys.stdout = imu.Logger(logPath)
-projectXmlPath = "%s/intermine/project.xml" % datasetPath
-
-imp.addSourcesToProject(
-    "%s/intermine/project.xml" % datasetPath,
+sbu.handleSimpleSourceAddProcess(
+    'promoters',
     [
         imp.Source(
             'dbtbs-regulation-Bsub168', 'synbio-regulation',
