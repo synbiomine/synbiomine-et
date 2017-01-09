fb34ab77e83af218ee86cecd5f4aec1fdc0d028e
Justin Clark-Casey <justincc@intermine.org>
2017-01-09 14:48:36 +0000

    correct python modules location on writeEcocycProjectXml.py

--- sources/ecocyc/writeEcocycProjectXml.py
+++ sources/ecocyc/writeEcocycProjectXml.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-sys.path.insert(1, os.path.dirname(os.path.abspath(__file__)) + '/../modules/python')
+sys.path.insert(1, os.path.dirname(os.path.abspath(__file__)) + '/../../modules/python')
 import intermyne.project as imp
 import intermyne.utils as imu
 
