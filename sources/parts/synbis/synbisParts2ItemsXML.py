3fd594707675da98a0270e4966b09a3d9fb9958c
Justin Clark-Casey <justincc@intermine.org>
2017-01-11 15:16:42 +0000

    print out component definitions from all files

--- sources/parts/synbis/synbisParts2ItemsXML.py
+++ sources/parts/synbis/synbisParts2ItemsXML.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
+import glob
 import jargparse
 import os
 import rdflib
+from rdflib.namespace import RDF
 import sys
 
 sys.path.insert(1, os.path.dirname(os.path.abspath(__file__)) + '/../../../modules/python')
@@ -22,8 +24,16 @@ ds.startLogging(__file__)
 
 partsPath = ds.getRawPath() + 'parts/'
 
-for partsFilename in os.listdir(partsPath):
+for partsFilename in glob.glob(partsPath + '*.xml'):
+# for partsFilename in os.listdir(partsPath):
+# for partsFilename in ['apFAB101.xml']:
     print('Analyzing ' + partsFilename)
-    with open(partsPath + partsFilename) as f:
+    with open(partsFilename) as f:
         g = rdflib.Graph()
         g.load(f)
+        # print(g.serialize(format='turtle').decode('unicode_escape'))
+        componentDefinitions = g.triples((None, RDF.type, rdflib.term.URIRef('http://sbols.org/v2#ComponentDefinition')))
+        # componentDefinitions = g.triples((None, rdflib.namespace.RDF.type, None))
+        #print(sum(1 for _ in componentDefinitions))
+        for componentDefinition in componentDefinitions:
+            print(componentDefinition)
