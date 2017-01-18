3edad3facaf6cf0882af80acfce1b410a5637a75
Justin Clark-Casey <justincc@intermine.org>
2017-01-18 12:13:59 +0000

    load all the synbis rdf into a single graph and print it out

--- sources/parts/synbis/buildOwlFromSynbis.py
+++ sources/parts/synbis/buildOwlFromSynbis.py
@@ -1,6 +1,37 @@
 #!/usr/bin/env python3
 
+import glob
+import jargparse
+import os
 import owlready as owr
+import rdflib
+import sys
+
+sys.path.insert(1, os.path.dirname(os.path.abspath(__file__)) + '/../../../modules/python')
+import intermyne.utils as imu
+import synbio.data as sbd
+
+############
+### MAIN ###
+############
+parser = jargparse.ArgParser('Take raw data downloaded from synbis and turn into InterMine Item XML.')
+parser.add_argument('colPath', help='path to the data collection.')
+parser.add_argument('-d', '--dummy', action='store_true', help='dummy run, do not store anything')
+parser.add_argument('-v', '--verbose', action='store_true', help='be verbose')
+args = parser.parse_args()
+
+dc = sbd.Collection(args.colPath)
+ds = dc.getSet('parts/synbis')
+ds.startLogging(__file__)
+
+g = rdflib.Graph()
+
+for partsPath in glob.glob(ds.getRawPath() + 'parts/*.xml'):
+    imu.printSection('Analyzing ' + partsPath)
+    with open(partsPath) as f:
+        g.load(f)
+
+print(g.serialize(format='turtle').decode('unicode_escape'))
 
 onto = owr.Ontology('http://dummy.owl')
 print(owr.to_owl(onto))
