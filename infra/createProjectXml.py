89d4c08355134a44a0719b7fef5ca13ecc32a797
Justin Clark-Casey <justincc@intermine.org>
2017-01-23 15:33:06 +0000

    print out the project xml writing scripts that we're executing

--- infra/createProjectXml.py
+++ infra/createProjectXml.py
@@ -4,6 +4,16 @@ import jargparse
 import shutil
 import subprocess
 
+#################
+### FUNCTIONS ###
+#################
+def writeProjectXml(path):
+    print('Executing ' + path)
+    subprocess.call([path, colPath])
+
+############
+### MAIN ###
+############
 parser = jargparse.ArgParser('Create the InterMine project XML for this data collection.')
 parser.add_argument('colPath', help='path to the data collection')
 
@@ -12,19 +22,19 @@ args = parser.parse_args()
 colPath = args.colPath
 
 shutil.copy('etc/project.xml.template', colPath + '/intermine/project.xml')
-subprocess.call(['./infra/writeSynbioMineProjectXml.pl',                colPath])
-subprocess.call(['./sources/ecocyc/writeEcocycProjectXml.py',           colPath])
-subprocess.call(['./sources/uniprot/writeUniprotProjectXml.py',         colPath])
-subprocess.call(['./sources/eggnog/writeEggNogProjectXml.py',           colPath])
-subprocess.call(['./sources/promoters/writePromotersProjectXml.py',     colPath])
-subprocess.call(['./sources/geo/writeGeoProjectXml.py',                 colPath])
-subprocess.call(['./sources/interpro/writeInterProProjectXml.py',       colPath])
-subprocess.call(['./sources/go/writeGoProjectXml.py',                   colPath])
-subprocess.call(['./sources/goa/writeGoaProjectXml.py',                 colPath])
-subprocess.call(['./sources/biogrid/writeBiogridProjectXml.py',         colPath])
-subprocess.call(['./sources/reactome/writeReactomeProjectXml.py',       colPath])
-subprocess.call(['./sources/orthodb/writeOrthoDbProjectXml.py',         colPath])
-subprocess.call(['./sources/path2model/writePath2ModelProjectXml.py',   colPath])
-subprocess.call(['./sources/parts/synbis/writeSynbisProjectXml.py',     colPath])
-subprocess.call(['./sources/pubmed/writePubMedProjectXml.py',           colPath])
-subprocess.call(['./sources/entrez/writeEntrezOrganismProjectXml.py',   colPath])
+writeProjectXml('./infra/writeSynbioMineProjectXml.pl')
+writeProjectXml('./sources/ecocyc/writeEcocycProjectXml.py')
+writeProjectXml('./sources/uniprot/writeUniprotProjectXml.py')
+writeProjectXml('./sources/eggnog/writeEggNogProjectXml.py')
+writeProjectXml('./sources/promoters/writePromotersProjectXml.py')
+writeProjectXml('./sources/geo/writeGeoProjectXml.py')
+writeProjectXml('./sources/interpro/writeInterProProjectXml.py')
+writeProjectXml('./sources/go/writeGoProjectXml.py')
+writeProjectXml('./sources/goa/writeGoaProjectXml.py')
+writeProjectXml('./sources/biogrid/writeBiogridProjectXml.py')
+writeProjectXml('./sources/reactome/writeReactomeProjectXml.py')
+writeProjectXml('./sources/orthodb/writeOrthoDbProjectXml.py')
+writeProjectXml('./sources/path2model/writePath2ModelProjectXml.py')
+writeProjectXml('./sources/parts/synbis/writeSynbisProjectXml.py')
+writeProjectXml('./sources/pubmed/writePubMedProjectXml.py')
+writeProjectXml('./sources/entrez/writeEntrezOrganismProjectXml.py')
