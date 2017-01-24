b50f9de0739dba4adb812bad067355e3cd8195d3
Justin Clark-Casey <justincc@intermine.org>
2017-01-24 16:41:17 +0000

    add fetchUniprotData.py.  This is going to take over from the corresponding code in fetchSynbioData.pl for better simplicity, maintainability and composability

---
+++ sources/uniprot/fetchUniprotData.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import jargparse
+import os
+import requests
+import sys
+
+sys.path.insert(1, os.path.dirname(os.path.abspath(__file__)) + '/../../modules/python')
+import intermyne.project as imp
+import synbio.data as sbd
+import synbio.utils as sbu
+
+############
+### MAIN ###
+############
+parser = jargparse.ArgParser('Fetch Uniprot XML for direct parsing by InterMine.')
+parser.add_argument('colPath', help='path to the dataset collection.')
+parser.add_argument('-v', '--verbose', action="store_true", help="verbose output")
+args = parser.parse_args()
+
+dc = sbd.Collection(args.colPath)
+ds = dc.getSet('uniprot')
+ds.startLogging(__file__)
+
+taxons = dc.getTaxons()
+
+print('Fetching %d taxons' % len(taxons))
+
+for taxon in taxons:
+    print('### Processing taxon: %s ###' % taxon)
+
+    sprotLocalPath = '%s/%s_uniprot_sprot.xml' % (ds.getLoadPath(), taxon)
+
+    # reviewed:yes (sprot) proteomes are human curated
+    resp = requests.get('http://www.uniprot.org/uniprot/?query=organism:%s+reviewed:yes&format=xml&include=yes' % (taxon))
+
+    with open(sprotLocalPath, 'w') as f:
+        f.write(resp.text)
+
+    tremblLocalPath = '%s/%s_uniprot_trembl.xml' % (ds.getLoadPath(), taxon)
+
+    # reviewed:no (trembl) proteomes are machine curated
+    resp = requests.get('http://www.uniprot.org/uniprot/?query=organism:%s+reviewed:no&format=xml&include=yes' % (taxon))
+
+    with open(tremblLocalPath, 'w') as f:
+        f.write(resp.text)
