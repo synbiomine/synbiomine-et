2ee177b7abdeebad28c6820388814f8673148960
Justin Clark-Casey <justincc@intermine.org>
2017-01-25 13:56:31 +0000

    adapt createCollection to create standard dataset structures and pull the genomic_model.xml from its own config rather than asking for a path

--- infra/createCollection.py
+++ infra/createCollection.py
@@ -6,12 +6,12 @@ import shutil
 import sys
 
 sys.path.insert(1, os.path.dirname(os.path.abspath(__file__)) + '/../modules/python')
-import intermyne.utils as imu
+import synbio.data as sbd
 
 #################
 ### CONSTANTS ###
 #################
-sections = [ 'eggnog', 'genbank', 'goa', 'intermine', 'kegg', 'taxons', 'uniprot' ]
+sections = [ 'eggnog', 'genbank', 'goa', 'kegg', 'taxons', 'uniprot' ]
 logsDir = 'logs'
 newDatasetSymlink = '../new'
 
@@ -19,11 +19,9 @@ newDatasetSymlink = '../new'
 ### MAIN ###
 ############
 parser = jargparse.ArgParser('Prepare a new data collection in the data repository')
-parser.add_argument('modelPath', help='path to the mine model XML')
-parser.add_argument('colPath', help='path to the data collection')
+parser.add_argument('colPath', help='path to where the new data collection should be')
 args = parser.parse_args()
 
-modelPath = args.modelPath
 colPath = args.colPath
 newDatasetSymlinkPath = "%s/%s" % (colPath, newDatasetSymlink)
 
@@ -31,14 +29,16 @@ if os.path.exists(colPath):
     raise Exception("Dataset path %s already exists!" % colPath)
 
 os.mkdir(colPath)
-os.mkdir("%s/%s" % (colPath, logsDir))
+os.mkdir('%s/%s' % (colPath, logsDir))
+os.mkdir('%s/%s' % (colPath, 'intermine'))
 
+shutil.copy('config/genomic_model.xml', '%s/intermine/genomic_model.xml' % colPath)
+
+dc = sbd.Collection(colPath)
+
+# Create the standard dataset structure in these paths
 for section in sections:
-    sectionPath = "%s/%s" % (colPath, section)
-    os.mkdir(sectionPath)
-
-datasetModelPath = "%s/intermine/genomic_model.xml" % colPath
-shutil.copy(modelPath, datasetModelPath)
+    ds = dc.getSet(section)
 
 os.symlink(os.path.basename(colPath), newDatasetSymlinkPath)
 
