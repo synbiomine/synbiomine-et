71383d78f63318dbc05b62c50e844722eea1cd02
Justin Clark-Casey <justincc@intermine.org>
2017-01-25 12:30:54 +0000

    update fetchGoData.py to python3

--- sources/go/fetchGoData.py
+++ sources/go/fetchGoData.py
@@ -1,17 +1,17 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
+import jargparse
 import os
 import requests
 import sys
 
 sys.path.insert(1, os.path.dirname(os.path.abspath(__file__)) + '/../../modules/python')
-import intermyne.utils as imu
 import synbio.data as sbd
 
 ############
 ### MAIN ###
 ############
-parser = imu.ArgParser('Fetch Gene Ontology files')
+parser = jargparse.ArgParser('Fetch Gene Ontology files')
 parser.add_argument('datasetPath', help='path to the dataset location')
 args = parser.parse_args()
 
@@ -20,9 +20,9 @@ ds = dc.getSet('go')
 ds.startLogging(__file__)
 
 uri = 'http://geneontology.org/ontology/go-basic.obo'
-destPath = "%s/%s" % (ds.getLoadPath(), uri.split('/')[-1])
+destPath = ds.getLoadPath() + uri.split('/')[-1]
 
-print "Fetching %s => %s" % (uri, destPath)
+print('Fetching %s => %s' % (uri, destPath))
 r = requests.get(uri)
 
 with open(destPath, 'w') as f:
